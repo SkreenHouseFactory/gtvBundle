0cb5d63856776d05ba4187b492cce2bd55c8ce17
bbergstorm <benoit@myskreen.com>
2012-08-13 20:38:04 +0200

    merge tv ok

--- Resources/public/js/script.js
+++ Resources/public/js/script.js
@@ -86,32 +86,100 @@ $(document).ready(function() {
 
   sideNavControl.showControl(showParams);
   */
+  var navSelectors = {item: '.item',
+                      itemParent: '.item-parent',
+                      itemRow: '.item-row'};
+  var selectionClasses = {basic: 'item-selected'};
 
   var zone_splash = new gtv.jq.KeyBehaviorZone({
     containerSelector: '#splash',
-    navSelectors: {
-      item: '.item',
-      itemParent: '.item-parent',
-      itemRow: '.item-row'
-    },
-    selectionClasses: {
-      basic: 'item-selected'
-    },
+    navSelectors: navSelectors,
+    selectionClasses: selectionClasses,
     keyMapping: {
       13: function(elmt){
-        console.log('script', 'enter callback', elmt);
-        UI.load(elmt.data('load-route'), elmt.data('load-view'));
+        console.log('script', 'zone_splash callback', elmt);
+        var args = {};
+        if (elmt.data('slider-scroll')) {
+          args.scroll = elmt.data('slider-scroll');
+        }
+        UI.load(elmt.data('load-route'), elmt.data('load-view'), args);
+        //elmt.click();
         return false;
       }
     }
   });
-  
   keyController.addBehaviorZone(zone_splash);
   keyController.start(zone_splash, true);
+
+
+  var zone_couchmode_slider = new gtv.jq.KeyBehaviorZone({
+    containerSelector: '.slider',
+    navSelectors: navSelectors,
+    selectionClasses: selectionClasses,
+    keyMapping: {
+      13: function(elmt){
+        console.log('script', 'zone_couchmode_slider callback', elmt.parent());
+        Couchmode.play(elmt.parent(), $('#couchmode-player'));
+        //elmt.click();
+        return false;
+      }
+    }
+  });
+  keyController.addBehaviorZone(zone_couchmode_slider);
+  keyController.start(zone_couchmode_slider, true);
+
+  //window.location= 'http://gri.gs/tv/gtv2/';
+
+  /*
+  var zone_couchmode_topbar = new gtv.jq.KeyBehaviorZone({
+    containerSelector: '#couchmode-topbar',
+    navSelectors: navSelectors,
+    selectionClasses: selectionClasses,
+    keyMapping: {
+      13: function(elmt){
+        console.log('script', 'zone_couchmode_topbar callback', elmt);
+        //elmt.click();
+        return false;
+      }
+    }
+  });
+  keyController.addBehaviorZone(zone_couchmode_topbar);
+  keyController.start(zone_couchmode_topbar, true);
+  */
+
+  /*var zone_couchmode_topbar_ul = new gtv.jq.KeyBehaviorZone({
+    containerSelector: '#couchmode-topbar ul',
+    navSelectors: navSelectors,
+    selectionClasses: selectionClasses,
+    keyMapping: {
+      13: function(elmt){
+        console.log('script', 'zone_couchmode_topbar_ul callback', elmt);
+        //elmt.click();
+        return false;
+      }
+    }
+  });
+  keyController.addBehaviorZone(zone_couchmode_topbar_ul);
+  keyController.start(zone_couchmode_topbar_ul, true);
+  */
 
   //keyController.stop();
   //keyController.removeAllZones();
 
   console.log('gtv', 'loaded');
+
+  /*
+  // Execute the decorator
+  try {
+    tv.ui.decorate($('#main'));
+  } catch (e) {
+    alert(e.message);
+  }
+  
+  // Set focus on initial element
+  var focusElement = $('.first-focus');
+  var focusComponent = tv.ui.getComponentByElement(focusElement);
+  focusComponent.tryFocus();
+  */
 
 });
