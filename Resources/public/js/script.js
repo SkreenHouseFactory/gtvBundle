14d41c7c8967ba1ff95c9219d37ea20193e1094f
bbergstorm <benoit@myskreen.com>
2012-10-02 17:41:36 +0200

    gotobrowser

--- Resources/public/js/script.js
+++ Resources/public/js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     Player.setType('android');
   }
 
+  Webview.launchAndKill('http://v3.benoit.myskreen.typhon.net/app_dev.php/gtv');
   //needed
   //if (typeof Webview != 'undefined') {
   //  Webview.postMessage(['fullscreen']);
